d97e9932b138de05adeba9a051ac15b3e0e10d42
Faisal Ilaiwi <faisal@intunedhq.com>
2026-03-02 13:58:39 -0800

    Bump hasura-dev formula to v0.1.3

--- Formula/hasura-dev.rb
+++ Formula/hasura-dev.rb
@@ -1,26 +1,26 @@
 class HasuraDev < Formula
   desc "Docker-free Hasura v2 development CLI runtime"
   homepage "https://github.com/faisalil/hasura-dev-cli"
-  version "0.1.2"
+  version "0.1.3"
   license "Apache-2.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/faisalil/hasura-dev-cli/releases/download/v0.1.2/hasura-dev-darwin-arm64.tar.gz"
-      sha256 "f0c50c4c7838443c259480a981d4270f23456c06cfce50b015d3bc14504478c7"
+      url "https://github.com/faisalil/hasura-dev-cli/releases/download/v0.1.3/hasura-dev-darwin-arm64.tar.gz"
+      sha256 "861efa33fd216acd836b308cdfd2dc82ebb7d18ac52be435dba199a036595d95"
     else
-      url "https://github.com/faisalil/hasura-dev-cli/releases/download/v0.1.2/hasura-dev-darwin-amd64.tar.gz"
-      sha256 "ddd349281db161797ee755a734f989e683fcdf58fe9eb42d8d3b707c4f20003b"
+      url "https://github.com/faisalil/hasura-dev-cli/releases/download/v0.1.3/hasura-dev-darwin-amd64.tar.gz"
+      sha256 "2d246219f5e856b0cfb032553be0510a29e6c1da576ad7635701ec3870df2a58"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/faisalil/hasura-dev-cli/releases/download/v0.1.2/hasura-dev-linux-arm64.tar.gz"
-      sha256 "7a686072e0fe9766f4ca2d6b4be756cfcabf3a74be517ecbdb6c1b8af393a081"
+      url "https://github.com/faisalil/hasura-dev-cli/releases/download/v0.1.3/hasura-dev-linux-arm64.tar.gz"
+      sha256 "e0cf3794ca902cb600461edd668df75a55ecb76339a9b8e1c0a3f09d588d5e73"
     else
-      url "https://github.com/faisalil/hasura-dev-cli/releases/download/v0.1.2/hasura-dev-linux-amd64.tar.gz"
-      sha256 "7b4ffc6dd47667fa5d6b6eedd1cee04bb9b83165e619efbc9e90c1c4241bf28c"
+      url "https://github.com/faisalil/hasura-dev-cli/releases/download/v0.1.3/hasura-dev-linux-amd64.tar.gz"
+      sha256 "111d37d5a192766b3662e749623e6224a277ccc833ce819b08250ad5a940d5f8"
     end
   end
 
